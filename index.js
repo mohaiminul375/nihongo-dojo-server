da5fba9ea57cef67b9cd47b2c9f5c1a01b66f23a
Mohaiminul375 <mohaiminul375@gmail.com>
2024-12-11 10:56:15 +0600

    register new user

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 5000
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const port = 5000;
+require('dotenv').config();
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+// Middleware & Cors
+app.use(express.json());
+// app.use();
+app.use(cors({ origin: 'http://localhost:3000' }));
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -20,6 +26,40 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
+        // collection
+        const usersCollections = client.db('nihongo-dojo').collection('all-users')
+
+
+
+        // user management
+        // register/ create a new user
+        app.post('/register', async (req, res) => {
+            const { user_name, email, img, password } = req.body;
+            console.log(req.body)
+            // field evaluation
+            if (!user_name || !email || !img || !password) {
+                return res.status(400).json({ error: 'All fields are required' });
+            }
+
+            try {
+                // is user existed
+                const isExisted = await usersCollections.findOne({ email });
+                if (isExisted) {
+                    return res.status(400).json({ error: 'Email already exists please login' });
+                }
+                // password has by bcrypt
+                const hasPassword = bcrypt.hash(password, 10);
+                // insert a new user 
+                const newUser = { user_name, email, img, password: hasPassword, role: 'User' }
+                const result = await usersCollections.insertOne(newUser);
+                // res send to front end
+                res.status(201).json({ message: 'User registered successfully' });
+            } catch (error) {
+                res.status(500).json({ error: 'Internal server error' });
+            }
+        })
+
+1
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -29,17 +69,6 @@ async function run() {
     }
 }
 run().catch(console.dir);
-
-
-
-
-
-
-
-
-
-
-
 
 // test server
 app.get('/', (req, res) => {
