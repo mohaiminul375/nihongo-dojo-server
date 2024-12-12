4b44c9296a1cb5d816d59b2389105f1fffaadde6
Mohaiminul375 <mohaiminul375@gmail.com>
2024-12-13 01:35:22 +0600

    delete lesson

--- index.js
+++ index.js
@@ -301,6 +301,13 @@ async function run() {
             }
         })
 
+        // delete lesson
+        app.delete('/all-lesson/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await lessonsCollections.deleteOne(query);
+            res.send(result)
+        })
 
 
 
